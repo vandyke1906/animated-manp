d8a900d8a6ce5cc70feed77b281dcd17c9fef035
vandyke1906 <vandyke1906@gmail.com>
2025-05-07 10:03:17 +0800

    added auto scroll on specific page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,6 +4,7 @@ import { TiLocationArrow } from "react-icons/ti";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
+
 gsap.registerPlugin(ScrollTrigger);
 
 const looping = false;
@@ -23,6 +24,18 @@ const Hero = () => {
     const handleVideoLoaded = () => {
         setLoadedVideos(prev => prev + 1)
     }
+
+    useEffect(() => {
+        const hash = window.location.hash; // Get the hash from URL
+        if (!isLoading && hash) {
+            const element = document.querySelector(hash);
+            if (element) {
+            element.scrollIntoView({ behavior: "smooth" });
+            }
+        }
+    }, [isLoading]);
+
+      
     useEffect(()=>{
         if(loadedVideos === totalVideos-1){
             setIsLoading(false)
@@ -71,69 +84,71 @@ const Hero = () => {
     })
     const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
     return (
-        <div id="home" className="relative h-dvh w-screen overflow-x-hidden" >
-            {isLoading && (
-                <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
-                  <div className="flex items-center justify-center h-full">
-                    <img
-                      src="/img/logo.png"
-                      alt="Mount Apo Geopark"
-                      className="animate-fade-up"
-                    />
-                  </div>
-                </div> 
-            )}
-            
-            <div id="video-frame" className="relative z-10 h-dvh w-screen overflow-hidden bg-blue-75" >
-                <div>
-                    <div className="mask-clip-path absolute-center z-50 size-64 cursor-pointer overflow-hidden rounded-lg" >
-                        <div onClick={handleMiniVideoClick} className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:opacity-100" >
-                            <video
-                                ref={nextVideoRef}
-                                src={getVideoSrc(upComingVideoIndex)}
-                                poster="img/fallback-background.jpg" // Add a fallback image here
-                                loop={looping}
-                                muted
-                                id="current-video"
-                                className="size-64 origin-center scale-150 object-cover object-center rounded-lg"
-                                onLoadedData={handleVideoLoaded}
-                                onEnded={() => setCurrentIndex((prev) => (prev % totalVideos) + 1)}
-                            />
+        <>
+            <div id="home" className="relative h-dvh w-screen overflow-x-hidden">
+                {isLoading && (
+                    <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
+                    <div className="flex items-center justify-center h-full">
+                        <img
+                        src="/img/logo.png"
+                        alt="Mount Apo Geopark"
+                        className="animate-fade-up"
+                        />
+                    </div>
+                    </div> 
+                )}
+                
+                <div id="video-frame" className="relative z-10 h-dvh w-screen overflow-hidden bg-blue-75" >
+                    <div>
+                        <div className="mask-clip-path absolute-center z-50 size-64 cursor-pointer overflow-hidden rounded-lg" >
+                            <div onClick={handleMiniVideoClick} className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:opacity-100" >
+                                <video
+                                    ref={nextVideoRef}
+                                    src={getVideoSrc(upComingVideoIndex)}
+                                    poster="img/fallback-background.jpg" // Add a fallback image here
+                                    loop={looping}
+                                    muted
+                                    id="current-video"
+                                    className="size-64 origin-center scale-150 object-cover object-center rounded-lg"
+                                    onLoadedData={handleVideoLoaded}
+                                    onEnded={() => setCurrentIndex((prev) => (prev % totalVideos) + 1)}
+                                />
+                            </div>
+                        </div>
+                        <video
+                            ref={nextVideoRef}
+                            src={getVideoSrc(currentIndex)}
+                            loop={looping}
+                            muted
+                            id="next-video"
+                            className="absolute-center invisible z-20 size-64 object-cover object-center"
+                            onLoadedData={handleVideoLoaded}
+                            onEnded={() => setCurrentIndex((prev) => (prev % totalVideos) + 1)}
+                        />
+                        <video
+                            src={getVideoSrc(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
+                            loop={looping}
+                            muted
+                            autoPlay
+                            className="absolute left-0 top-0 z-10 size-full object-cover object-center"
+                            onLoadedData={handleVideoLoaded}
+                            onEnded={() => setCurrentIndex((prev) => (prev % totalVideos) + 1)}
+                        />
+                    </div>
+                    <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75" ><b>ASEAN</b> Heritage Park</h1>
+                    <div className="absolute left-0 top-0 z-40 size-full" >
+                        <div className="mt-24 px-5 sm:px-10" >
+                            <h1 className="special-font hero-heading text-blue-100" >MT. <b>APO</b> GEOPARK</h1>
+                            <p className="mb-5 max-w-128 font-robert-regular text-blue-100">
+                                Highest mountain peak in the Philippines with an elevation of 2,954 meters above sea level<br />and a land area of 65,900.9 hectares. 
+                            </p>
+                            <Button id='watch- trailer' title='Read More' leftIcon={<TiLocationArrow />} containerClass='bg-blue-300 flex-center gap-1' />
                         </div>
                     </div>
-                    <video
-                        ref={nextVideoRef}
-                        src={getVideoSrc(currentIndex)}
-                        loop={looping}
-                        muted
-                        id="next-video"
-                        className="absolute-center invisible z-20 size-64 object-cover object-center"
-                        onLoadedData={handleVideoLoaded}
-                        onEnded={() => setCurrentIndex((prev) => (prev % totalVideos) + 1)}
-                    />
-                    <video
-                        src={getVideoSrc(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
-                        loop={looping}
-                        muted
-                        autoPlay
-                        className="absolute left-0 top-0 z-10 size-full object-cover object-center"
-                        onLoadedData={handleVideoLoaded}
-                        onEnded={() => setCurrentIndex((prev) => (prev % totalVideos) + 1)}
-                    />
                 </div>
-                <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75" ><b>ASEAN</b> Heritage Park</h1>
-                <div className="absolute left-0 top-0 z-40 size-full" >
-                    <div className="mt-24 px-5 sm:px-10" >
-                        <h1 className="special-font hero-heading text-blue-100" >MT. <b>APO</b> GEOPARK</h1>
-                        <p className="mb-5 max-w-128 font-robert-regular text-blue-100">
-                            Highest mountain peak in the Philippines with an elevation of 2,954 meters above sea level<br />and a land area of 65,900.9 hectares. 
-                        </p>
-                        <Button id='watch- trailer' title='Read More' leftIcon={<TiLocationArrow />} containerClass='bg-blue-300 flex-center gap-1' />
-                    </div>
-                </div>
+                <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black" ><b>ASEAN</b> Heritage Park</h1>
             </div>
-            <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black" ><b>ASEAN</b> Heritage Park</h1>
-        </div>
+        </>
     )
 };
 
