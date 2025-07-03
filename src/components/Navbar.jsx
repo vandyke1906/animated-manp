62735784f024b8c6abcf0935c040592afbb5bc2f
vandyke1906 <vandyke1906@gmail.com>
2025-07-03 10:02:33 +0800

    updated map

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -54,7 +54,7 @@ const Navbar = ({ isHome = true }) => {
 
     }, [isAudioPlaying])
     return (
-        <div ref={navContainerRef} className="fixed inset-x-0 top-4 h-16 border-none transition-all duration-700 sm:inset-x-6 z-500" >
+        <div ref={navContainerRef} className="fixed inset-x-0 top-4 h-16 border-none transition-all duration-700 sm:inset-x-6 z-50" >
             <header className="absolute top-1/2 w-full -translate-y-1/2" >
                 <nav className="flex size-full items-center justify-between p-4" >
                     <div className="flex items-center gap-5" >
--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -13,25 +13,18 @@ const Map = () => {
   return (
     <>
       <Navbar isHome={false} />
-      <div className="mt-20 mb-4 min-h-96 w-screen px-10 overflow-y-auto no-scrollbar">
-        {/* <div className="py-10">
-          <h1 className="special-font hero-heading text-center primary-color" ><b>Mount Apo</b> Map</h1>
-        </div> */}
-        <div className="container mx-auto">
-          <MapContainer center={[latitude, longitude]} zoom={11} ref={mapRef} style={{height: "70vh", width: "90vw"}} bounds={MountApoCoordinates} boundsOptions={{ padding: [1, 1] }}>
-              <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              />
-              <Marker position={[latitude, longitude]}>
-                  <Popup>
-                  Mount Apo Natural Park
-                  </Popup>
-              </Marker>
-                <Polygon positions={MountApoCoordinates} />
-            </MapContainer>
-        </div>
-      </div>
+      <MapContainer center={[6.95, 125.2792]} zoom={12} ref={mapRef} style={{height: "100vh", width: "100vw"}} bounds={MountApoCoordinates} boundsOptions={{ padding: [1, 1] }} className="z-10" scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Polygon positions={MountApoCoordinates} />
+        <Marker position={[latitude, longitude]}>
+            <Popup>
+            Mount Apo Natural Park
+            </Popup>
+        </Marker>
+      </MapContainer>
       <Footer />
     </>
   )
